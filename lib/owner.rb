f1dc1836056bbf624b343188ee25d6a19d4aa258
Jonathan Norman <jnorman2123@gmail.com>
2019-10-22 16:21:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,8 +3,8 @@ class Owner
 attr_accessor :species
   attr_reader :name
 
-  def initialize(name, species)
+  def initialize(name)
     @name = name
-    @species = species
+    @species
   end
 end
