39f409dff57d6eeabeaf76e8804f616b71bb08bb
Jonathan Norman <jnorman2123@gmail.com>
2019-10-22 16:55:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,7 +28,7 @@ class Owner
   end
 
   def cats
-    Cat.all.select{|cat| cat.owner == self}
+    @cats = Cat.all.select{|cat| cat.owner == self}
   end
 
   def dogs
@@ -36,6 +36,6 @@ class Owner
   end
 
   def buy_cat(cat)
-    self.cats << cat
+    @cats << cat
   end
 end
